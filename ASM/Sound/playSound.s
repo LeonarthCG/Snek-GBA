f98abf8db3de1cbcbe4cda08ac87313e10fc6f1a
LeonarthCG <leonarth.cg@gmail.com>
2019-02-10 01:47:35 +0100

    added sound effect routine

--- ASM/Sound/playSound.s
+++ ASM/Sound/playSound.s
@@ -2,49 +2,49 @@
 
 mov	r3,r0		@song to play
 
-@reset FIFO_A
+@reset FIFO_B
 ldr	r0,=#0x4000080
 ldrh	r1,[r0,#2]
-ldr	r2,=#0x800
+ldr	r2,=#0x8000
 orr	r1,r2
 strh	r1,[r0,#2]
 
-@stop current DMA1
-ldr	r0,=#0x40000BC
+@stop current DMA2
+ldr	r0,=#0x40000C8
 mov	r1,#0
 strh	r1,[r0,#10]
 
-@set DMA1 source
+@set DMA2 source
 ldr	r1,[r3,#8]
 str	r1,[r0]
 
-@set DMA1 destination
-ldr	r1,=#0x40000A0
+@set DMA2 destination
+ldr	r1,=#0x40000A4
 str	r1,[r0,#4]
 
-@set DMA1 count, not used but whatever
+@set DMA2 count, not used but whatever
 ldr	r1,[r3,#4]
 lsr	r1,#2
 strh	r1,[r0,#8]
 
-@set DMA1 control
+@set DMA2 control
 ldr	r1,=#0xB640
 strh	r1,[r0,#10]
 
 @set the sample rate
 ldr	r0,=#0x4000100
 ldr	r1,[r3]
-strh	r1,[r0]
+strh	r1,[r0,#4]
 
-@enable timer 0
+@enable timer 1
 ldr	r1,=#0xC0
-strh	r1,[r0,#2]
+strh	r1,[r0,#6]
 
 ldr	r0,=soundCounters
 mov	r1,#0
-ldr	r2,[r0]
+ldr	r2,[r0,#8]
 str	r1,[r2]		@set played samples to 0
-ldr	r2,[r0,#4]
+ldr	r2,[r0,#12]
 str	r3,[r2]		@set song pointer
 
 bx	lr
--- ASM/interrupt.s
+++ ASM/interrupt.s
@@ -13,10 +13,27 @@ ldrh	r1,[r0,#2]
 mov	r0,#1
 and	r0,r1
 cmp	r0,#1
-beq	wasvblank
+bne	notvblank
+bl	wasvblank
+notvblank:
+mov	r0,#8
+and	r0,r1
+cmp	r0,#8
+bne	notcounter0
+bl	wascounter0
+notcounter0:
+mov	r0,#16
+and	r0,r1
+cmp	r0,#16
+bne	notcounter1
+bl	wascounter1
+notcounter1:
+b	End
 
 @if timer 0, increase samples played, if samples are over end song
 wascounter0:
+push	{lr}
+push	{r1}
 ldr	r0,=soundCounters
 ldr	r2,[r0]
 ldr	r1,[r2]		@increase played samples
@@ -33,30 +50,85 @@ ldr	r0,=#0x40000BC
 mov	r1,#0
 strh	r1,[r0,#10]
 @stop timer 0 interrupts
-	@ - !!! -
+ldr	r0,=#0x4000100
+mov	r1,#0
+strh	r1,[r0,#2]
 @start next song (or loop)
 ldr	r0,=soundCounters
 ldr	r0,[r0,#4]
 ldr	r0,[r0]		@next song data
 cmp	r0,#0
+ldr	r0,[r0,#12]
 beq	endcounter0
 ldr	r3,=playSong
 mov	lr,r3
 .short	0xF800
 endcounter0:
 mov	r0,#8
-b	End
-
-
-@if vblank, just acknowledge and return
-wasvblank:
-mov	r0,#1
-b	End
-
-End:
 ldr	r1,=#0x04000200		@mark the bit for the interrupt
 strh	r0,[r1,#2]
 ldr	r1,=#0x03007FF8		@also mark the bit for the interrupt, I think the previous one is unneeded
 strh	r0,[r1]
+pop	{r1}
+pop	{r0}
+bx	r0
+
+@if timer 1, increase samples played, if samples are over end sound
+wascounter1:
+push	{lr}
+push	{r1}
+ldr	r0,=soundCounters
+ldr	r2,[r0,#8]
+ldr	r1,[r2]		@increase played samples
+add	r1,#1
+str	r1,[r2]
+ldr	r2,[r0,#12]
+ldr	r2,[r2]		@load sound pointer to check sample ammount
+ldr	r2,[r2,#4]
+cmp	r1,r2
+blo	endcounter1
+@stop sound playback
+@set DMA2 control
+ldr	r0,=#0x40000C8
+mov	r1,#0
+strh	r1,[r0,#10]
+@stop timer 1 interrupts
+ldr	r0,=#0x4000100
+mov	r1,#0
+strh	r1,[r0,#6]
+@start next sound (or loop)
+ldr	r0,=soundCounters
+ldr	r0,[r0,#12]
+ldr	r0,[r0]		@next sound data
+ldr	r0,[r0,#12]
+cmp	r0,#0
+beq	endcounter1
+ldr	r3,=playSound
+mov	lr,r3
+.short	0xF800
+endcounter1:
+mov	r0,#16
+ldr	r1,=#0x04000200		@mark the bit for the interrupt
+strh	r0,[r1,#2]
+ldr	r1,=#0x03007FF8		@also mark the bit for the interrupt, I think the previous one is unneeded
+strh	r0,[r1]
+pop	{r1}
+pop	{r0}
+bx	r0
+
+@if vblank, just acknowledge and return
+wasvblank:
+push	{lr}
+push	{r1}
+mov	r0,#1
+ldr	r1,=#0x04000200		@mark the bit for the interrupt
+strh	r0,[r1,#2]
+ldr	r1,=#0x03007FF8		@also mark the bit for the interrupt, I think the previous one is unneeded
+strh	r0,[r1]
+pop	{r1}
+pop	{r0}
+bx	r0
+
+End:
 pop	{r0}
 bx	r0
